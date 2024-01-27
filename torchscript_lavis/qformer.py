6f472e18054205d7801d35ba26d9f881f1718c59
Masaya Kataoka <ms.kataoka@gmail.com>
2024-01-27 11:03:32 +0900

    remove unused layer in image encoder
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- torchscript_lavis/qformer.py
+++ torchscript_lavis/qformer.py
@@ -12,6 +12,7 @@ from typing import Optional
 from PIL import Image
 import os
 import torchscript_lavis
+import time
 
 
 class Blip2TextEncoder(torch.nn.Module):
@@ -85,31 +86,53 @@ class Blip2ImageEncoder(torch.nn.Module):
     def __init__(self):
         super().__init__()
         self.device = torch.device("cuda") if torch.cuda.is_available() else "cpu"
-        self.model, _, _ = load_model_and_preprocess(
+        model, _, _ = load_model_and_preprocess(
             "blip2_image_text_matching", "pretrain", device=self.device, is_eval=True
         )
         self.image_processor = Blip2ImageProcessor(image_size=224)
+        self.maybe_autocast = model.maybe_autocast
+        self.ln_vision = model.ln_vision
+        self.visual_encoder = model.visual_encoder
+        self.query_tokens = model.query_tokens
+        self.bert = model.Qformer.bert
+        self.vision_proj = model.vision_proj
 
     def forward(self, image_tensor: torch.Tensor) -> torch.Tensor:
         image = self.image_processor(image_tensor)
         assert image.dim() == 3
         image = image.unsqueeze(0).to(self.device)
-        with self.model.maybe_autocast():
-            image_embeds = self.model.ln_vision(self.model.visual_encoder(image))
+        with self.maybe_autocast():
+            image_embeds = self.ln_vision(self.visual_encoder(image))
         image_embeds = image_embeds.float()
         image_atts = torch.ones(image_embeds.size()[:-1], dtype=torch.long).to(
             image.device
         )
-        query_tokens = self.model.query_tokens.expand(image_embeds.shape[0], -1, -1)
-        query_output = self.model.Qformer.bert(
+        query_tokens = self.query_tokens.expand(image_embeds.shape[0], -1, -1)
+        query_output = self.bert(
             query_embeds=query_tokens,
             encoder_hidden_states=image_embeds,
             encoder_attention_mask=image_atts,
             return_dict=True,
         )
-        return F.normalize(
-            self.model.vision_proj(query_output.last_hidden_state), dim=-1
+        return F.normalize(self.vision_proj(query_output.last_hidden_state), dim=-1)
+
+    def inference(
+        self, model_filename: str = "qformer_image_encoder.pt"
+    ) -> torch.Tensor:
+        torchscript_model = torch.jit.load(model_filename)
+        image_tensor = self.image_processor(
+            self.image_to_tensor(
+                Image.open(
+                    os.path.join(torchscript_lavis.__path__[0], "merlion_demo.png")
+                )
+            )[0:3, :, :]
         )
+        # start = time.time()
+        # out = model(image_tensor)
+        # end = time.time()
+        # print(end - start)
+        # print(model)
+        return torchscript_model(image_tensor)
 
     def image_to_tensor(self, image: Image) -> torch.Tensor:
         return transforms.Compose([transforms.ToTensor()])(image)
@@ -122,12 +145,26 @@ class Blip2ImageEncoder(torch.nn.Module):
                 )
             )[0:3, :, :]
         )
+        # print(image_tensor.size())
         traced_model = torch.jit.trace(self, (image_tensor))
         traced_model.save(model_filename)
+        # print(traced_model)
 
+    def optimize_and_save(
+        self,
+        model_filename: str = "qformer_image_encoder.pt",
+        model_out_filename: str = "qformer_image_encoder_optimize.pt",
+    ):
+        model = torch.jit.load(model_filename)
+        torch.jit.optimize_for_inference(torch.jit.script(model.eval())).save(
+            model_out_filename
+        )
 
 if __name__ == "__main__":
-    Blip2TextEncoder().trace()
+    # Blip2ImageEncoder()
+    # Blip2TextEncoder().trace()
     Blip2ImageEncoder().trace()
+    # print(Blip2ImageEncoder().inference())
+    Blip2ImageEncoder().optimize_and_save()
     # Blip2TextEncoder().inference("Hello world, for testing text encoder.")
     pass
